405a8cbce4aa4fe1e56d7d0007491df8f7ed4ace
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-27 15:25:52 -0700

    create basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Server...");
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: "Welcome to support desk api" })
+})
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
