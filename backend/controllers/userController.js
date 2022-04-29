90c553c584b31b59d2e63d1d701654ab0a4b9af2
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-28 18:11:41 -0700

    protect current user route, using jsonwebtokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,6 +73,18 @@ const loginUser = asyncHandler(async (req, res) => {
 
 })
 
+// @decs  Get current user
+// @route /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name
+  }
+  res.status(200).json(user)
+})
+
 // Generate Token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -81,5 +93,5 @@ const generateToken = (id) => {
 }
 
 module.exports = {
-  registerUser, loginUser
+  registerUser, loginUser, getMe
 }
