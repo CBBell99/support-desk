66057b0613733f6f1886270c3902d240e2e83556
CBBell99 <chrisbeckertonbell@gmail.com>
2022-04-30 22:55:42 -0600

    create authService function

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import authService from "./authService"
 
 const initialState = {
   user: null,
@@ -9,9 +10,20 @@ const initialState = {
 }
 
 // Register new user
-export const register = createAsyncThunk("auth/register", async (user, thunkApi) => {
-  console.log(user)
-})
+export const register = createAsyncThunk("auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user)
+    } catch (error) {
+      const message = (error.response &&
+        error.response.data
+        && error.response.data.message) ||
+        error.message ||
+        error.toString()
+
+      return thunkAPI.rejectWithValue(message)
+    }
+  })
 
 
 // Login new user
