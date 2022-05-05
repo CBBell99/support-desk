6ae0bcaefd62a9b2c305062aff612e2482cb3255
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-05 06:52:35 -0700

    get tickets from backend

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const API_URL = '/api/tickets/'
 
 // Create new ticket
-
 const createTicket = async (ticketData, token) => {
   const config = {
     headers: {
@@ -12,11 +11,26 @@ const createTicket = async (ticketData, token) => {
   }
 
   const response = await axios.post(API_URL, ticketData, config)
+
+  return response.data
+}
+
+// Get user tickets
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }
+
+  const response = await axios.get(API_URL, config)
+
   return response.data
 }
 
 const ticketService = {
-  createTicket
+  createTicket,
+  getTickets
 }
 
 export default ticketService
