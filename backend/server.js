f0f29755ce00970be84beb28b349205c29df7b49
CBBell99 <chrisbeckertonbell@gmail.com>
2022-05-10 08:17:02 -0700

    fix typo

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-  app.get("*", (req, res) => res.sendFile(__dirname, "../frontend/buildindex.html"))
+  app.get("*", (req, res) => res.sendFile(__dirname, "../frontend/build/index.html"))
 } else {
   app.get('/', (req, res) => {
     res.status(200).json({ message: "Welcome to support desk api" })
